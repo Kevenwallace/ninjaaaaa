e600890a4fc98e63b596634b9595339e3c2217a1
keven wallace <kevenwallace_@hotmail.com>
2025-08-13 01:36:48 -0300

    Initial Commit

---
+++ interface recognizeTests/interface_recognizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  interface_recognizeTests.swift
+//  interface recognizeTests
+//
+//  Created by keven wallace dutra de macedo on 13/08/25.
+//
+
+import Testing
+@testable import interface_recognize
+
+struct interface_recognizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
